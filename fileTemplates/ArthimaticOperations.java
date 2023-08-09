825df2c66e3706284f2835cefeba8ea00798e9ac
rohitpalli28 <rohit.palli@ula.app>
2023-08-09 15:13:07 +0530

    IC-2022.1 <rohitpalli@Rohits-MacBook-Pro.local Create FeatureSuggester.xml, laf.xml, ide-features-trainer.xml, project.default.xml, ArthimaticOperations.java, jdk.table.xml, shared-indexes.xml, Default.xml, find.xml, ignored-suggested-plugins.xml, DontShowAgainFeedbackService.xml, filetypes.xml, settingsSyncPlugins.xml, vcs.xml, editor-font.xml, colors.scheme.xml, editor.xml, github.xml, debugger.xml, updates.xml

---
+++ fileTemplates/ArthimaticOperations.java
@@ -0,0 +1,29 @@
+package ${PACKAGE_NAME};
+
+public class ${NAME} {
+
+    public static void main(String[] args) {
+        int num1,num2,sum,difference,product,quotient;
+        System.out.println("Input -");
+        num1 = 10;
+        num2 = 5;
+
+        System.out.println("num1 = "+num1);
+        System.out.println("num2 = "+num2);
+        sum = num1+num2;
+        difference= num1-num2;
+        product = num1*num2;
+        quotient =num1/num2;
+
+        System.out.println("The sum of "+num1+" and "+num2+" "+"is "+sum);
+        System.out.println("The difference between "+num1+" and "+num2+" "+"is "+difference);
+        System.out.println("The product of "+num1+" and "+num2+" "+"is "+product);
+        System.out.println("The quotient of "+num1+" divided by  "+num2+" "+"is "+product);
+
+        //The sum of 10 and 5 is 15.
+        //The difference between 10 and 5 is 5. The product of 10 and 5 is 50.
+        //The quotient of 10 divided by 5 is 2.
+
+
+    }
+}
